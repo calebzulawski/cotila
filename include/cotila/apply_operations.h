2481a74d2d5c65a0203672ee438d6c0cddcb3df5
Caleb Zulawski <caleb.zulawski@gmail.com>
2018-02-11 10:21:32 -0500

    Simplify unary and binary operation detail

--- include/cotila/apply_operations.h
+++ include/cotila/apply_operations.h
@@ -9,40 +9,25 @@
 namespace cotila {
 namespace detail {
 
-template <typename F, typename T, std::size_t N, std::size_t... I>
-constexpr decltype(auto) elementwise_unary_impl(F &&f, const vector<T, N> &v,
-                                                std::index_sequence<I...>) {
-  return make_vector(std::apply(
-      std::forward<F>(f), std::forward_as_tuple(std::get<I>(v.to_array())))...);
-}
-
 template <typename F, typename T, std::size_t N, typename... Args>
 constexpr decltype(auto) elementwise_unary(F f, const vector<T, N> &v,
                                            Args &&... args) {
-  auto op = [f, args = std::forward_as_tuple(args...)](T x) {
-    return std::apply(f, std::tuple_cat(std::forward_as_tuple(x), args));
-  };
-  return elementwise_unary_impl(op, v, std::make_index_sequence<N>{});
-}
-
-template <typename F, typename T, std::size_t N, std::size_t... I>
-constexpr decltype(auto) elementwise_binary_impl(F &&f, const vector<T, N> &v1,
-                                                 const vector<T, N> &v2,
-                                                 std::index_sequence<I...>) {
-  return make_vector(
-      std::apply(std::forward<F>(f),
-                 std::forward_as_tuple(std::get<I>(v1.to_array()),
-                                       std::get<I>(v2.to_array())))...);
+  using ret_type = std::invoke_result_t<F, T, Args...>;
+  std::array<ret_type, N> op_applied = {};
+  for (std::size_t i = 0; i < N; ++i)
+    op_applied[i] = std::apply(f, std::forward_as_tuple(v[i], args...));
+  return op_applied;
 }
 
 template <typename F, typename T, std::size_t N, typename... Args>
 constexpr decltype(auto) elementwise_binary(F f, const vector<T, N> &v1,
                                             const vector<T, N> &v2,
                                             Args &&... args) {
-  auto op = [f, args = std::forward_as_tuple(args...)](T x, T y) {
-    return std::apply(f, std::tuple_cat(std::forward_as_tuple(x, y), args));
-  };
-  return elementwise_binary_impl(op, v1, v2, std::make_index_sequence<N>{});
+  using ret_type = std::invoke_result_t<F, T, T, Args...>;
+  std::array<ret_type, N> op_applied = {};
+  for (std::size_t i = 0; i < N; ++i)
+    op_applied[i] = std::apply(f, std::forward_as_tuple(v1[i], v2[i], args...));
+  return op_applied;
 }
 
 } // namespace detail
