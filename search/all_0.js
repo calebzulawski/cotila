5db1ce3faa50a0381ce4ae09cba2467b8e5e43b1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-03-05 03:23:22 +0000

    Deploy calebzulawski/cotila to github.com/calebzulawski/cotila.git:gh-pages

--- search/all_0.js
+++ search/all_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['abs',['abs',['../scalar_2math_8h_a39a84d83686d47472d9bccbcb543341d.html#a39a84d83686d47472d9bccbcb543341d',1,'cotila::abs(T x)'],['../scalar_2math_8h_af823d66606fbf52482dc8fe5cf5dea5b.html#af823d66606fbf52482dc8fe5cf5dea5b',1,'cotila::abs(T x)'],['../vector_2math_8h_a583040f31dcf5ed1bfa86de7b70a4aff.html#a583040f31dcf5ed1bfa86de7b70a4aff',1,'cotila::abs(const vector&lt; T, N &gt; &amp;v)']]]
+  ['abs',['abs',['../scalar_2math_8h_a0491eb629909827138858260eb717f0f.html#a0491eb629909827138858260eb717f0f',1,'cotila::abs(T x)'],['../vector_2math_8h_a21cd31f05627bca4abbd0d9453234df0.html#a21cd31f05627bca4abbd0d9453234df0',1,'cotila::abs(const vector&lt; T, N &gt; &amp;v)']]]
 ];
